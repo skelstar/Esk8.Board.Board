5c6c168e7e34e170c7e82bb9a09cf32742917767
sean-kelly <skelstar@gmail.com>
2019-09-19 07:26:44 +1200

    Moved LED stuff to a sep library. Nice and tidy!

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,10 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include "LedLightsLib.h"
 
 /*--------------------------------------------------------------------------------*/
 
-#define NUM_PIXELS  15
-#define PIXEL_PIN  32
-
-#define BRIGHTNESS  100
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIXEL_PIN, NEO_GRBW + NEO_KHZ800);
-
-uint32_t COLOUR_OFF = strip.Color(0, 0, 0, 0);
-uint32_t COLOUR_RED = strip.Color(0, 255, 0, 0);
-uint32_t COLOUR_GREEN = strip.Color(0, 255, 0);
-uint32_t COLOUR_BLUE = strip.Color(0, 0, 255, 0);
-uint32_t COLOUR_WHITE = strip.Color(0, 0, 30, 255);
-
-
-void setPixel(int i, uint32_t c, bool show);
-void setAllPixels(uint32_t c);
+LedLightsLib strip(/*pin*/ 32, /*num*/ 15);
 
 /*--------------------------------------------------------------------------------*/
 
@@ -32,26 +18,10 @@ void setup()
 	Serial.begin(115200);
   Serial.println("\nStarting Esk8.Board.Server!");
 
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
-  setAllPixels(COLOUR_WHITE);
+  strip.setAll(strip.COLOUR_WHITE);
 }
 
 void loop() {
 
   delay(10);
-}
-
-void setPixel(int i, uint32_t c, bool show) {
-  strip.setPixelColor(i, c);
-  if (show) {
-    strip.show();
-  }
-}
-
-void setAllPixels(uint32_t c) {
-  for (int i=0; i<NUM_PIXELS; i++) {
-    setPixel(i, c, false);
-  }
-  strip.show();
 }
