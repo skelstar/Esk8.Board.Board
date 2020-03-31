0d9d2151d597197807967a7bc726537806772dbb
Sean <skelstar@gmail.com>
2020-03-31 16:55:03 +1300

    More pixels for ring lights.

--- include/peripherals.h
+++ include/peripherals.h
@@ -4,8 +4,8 @@
 #include <Button2.h>
 
 #define BUTTON_0 0
-#define PIXEL_PIN 13 //4
-#define NUM_PIXELS 21
+#define PIXEL_PIN 13  //4
+#define NUM_PIXELS 13 // per ring
 
 Button2 button0(BUTTON_0);
 
@@ -13,6 +13,7 @@ void button_init()
 {
   button0.setPressedHandler([](Button2 &btn) {
     board_packet.moving = true;
+    board_packet.motorCurrent += 0.1;
   });
   button0.setReleasedHandler([](Button2 &btn) {
     board_packet.odometer = board_packet.odometer + 0.1;
@@ -25,6 +26,6 @@ void button_init()
 //------------------------------------------------------------------
 void light_init()
 {
-  light.initialise(PIXEL_PIN, NUM_PIXELS);
-  light.setAll(light.COLOUR_OFF);
+  light.initialise(PIXEL_PIN, NUM_PIXELS * 2, 100);
+  light.setAll(light.COLOUR_BLUE);
 }
