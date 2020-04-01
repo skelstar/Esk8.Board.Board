280af56a350bc9d94942c23e8f70313fc54553a5
Sean <skelstar@gmail.com>
2020-04-01 16:40:07 +1300

    Fixed all leds not coming on (o-based index).

--- src/lights_play/lights_play.cpp
+++ src/lights_play/lights_play.cpp
@@ -6,19 +6,23 @@
 #include <elapsedMillis.h>
 
 //------------------------
-#define PIXEL_PIN 26
+#define PIXEL_PIN 13
 #define NUM_PIXELS 13 // per ring
 //------------------------
 
 #include <LedLightsLib.h>
 LedLightsLib lights;
 
+#include <Adafruit_NeoPixel.h>
+
+Adafruit_NeoPixel strip(NUM_PIXELS * 2, PIXEL_PIN, NEO_GRBW + NEO_KHZ800);
+
 void setup()
 {
   Serial.begin(115200);
   DEBUG("Ready");
 
-  lights.initialise(PIXEL_PIN, NUM_PIXELS * 2, /*brightness*/ 100);
+  lights.initialise(PIXEL_PIN, NUM_PIXELS * 2, /*brightness*/ 30);
   lights.setAll(lights.COLOUR_COLD_WHITE);
 }
 
