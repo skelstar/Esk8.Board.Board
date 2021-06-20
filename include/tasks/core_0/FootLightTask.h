7befed1ed9b9240e1a736151ac9a9b56b74c8951
Sean <skelstar@gmail.com>
2021-06-20 17:49:26 +1200

    Started working on battery display when disconncected.

--- include/tasks/core_0/FootLightTask.h
+++ include/tasks/core_0/FootLightTask.h
@@ -46,14 +46,17 @@ class FootLightTask : public TaskBase
   }
 
 public:
-  VescData vescData;
-
   bool printStateChange = false;
 
   LedLightsLib lightStrip;
 
 private:
   Queue1::Manager<VescData> *vescDataQueue = nullptr;
+  Queue1::Manager<ControllerData> *controllerQueue = nullptr;
+
+  VescData m_vescData;
+  ControllerData m_controllerData;
+  elapsedMillis sinceLastControllerData = 1000;
 
 public:
   FootLightTask() : TaskBase("FootLightTask", 3000)
@@ -66,22 +69,47 @@ private:
   {
     vescDataQueue = createQueue<VescData>("vescDataQueue");
     vescDataQueue->printMissedPacket = false;
+
+    controllerQueue = createQueue<ControllerData>("controllerQueue");
+    controllerQueue->printMissedPacket = true;
+
     lightStrip.initialise(FOOTLIGHT_PIXEL_PIN, FOOTLIGHT_NUM_PIXELS, FOOTLIGHT_BRIGHTNESS_STOPPED);
     lightStrip.setAll(lightStrip.COLOUR_DARK_RED);
   }
 
   void doWork()
   {
+    using namespace nsFootlightTask;
+
     if (vescDataQueue->hasValue())
-    {
-      vescData = vescDataQueue->payload;
-      nsFootlightTask::updateLights(vescData);
-    }
-  } // doWork
+      _handleVescData(vescDataQueue->payload);
+
+    if (controllerQueue->hasValue())
+      _handleControllerData(controllerQueue->payload);
+
+    // bool connected = sinceLastControllerData > 400;
+
+    // if (connected)
+    //   nsFootlightTask::state = nsFootlightTask::DISCONNECTED;
+
+    updateLights(m_vescData);
+  }
 
   void cleanup()
   {
     delete (vescDataQueue);
+    delete (controllerQueue);
+  }
+
+private:
+  void _handleVescData(const VescData &payload)
+  {
+    m_vescData = vescDataQueue->payload;
+  }
+
+  void _handleControllerData(const ControllerData &payload)
+  {
+    sinceLastControllerData = 0;
   }
 };
 //-------------------------------------------------------
@@ -98,8 +126,10 @@ namespace nsFootlightTask
 
   const uint8_t STOPPED = 0;
   const uint8_t MOVING = 1;
-  const uint8_t UNKNOWN = 2;
+  const uint8_t DISCONNECTED = 2;
+  const uint8_t UNKNOWN = 3;
   uint8_t moving = UNKNOWN;
+  uint8_t state = DISCONNECTED;
 
   void updateLights(const VescData &vescData)
   {
